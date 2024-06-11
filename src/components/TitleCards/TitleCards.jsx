e67b9bea5ea7d309fcafd97e9780f128b7b3093c
Renzxs <florencebatol85@gmail.com>
2024-06-11 18:32:58 +0800

    Added Cards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import CardsData from '../../assets/assets/cards/Cards_data'
 
 const TitleCards = () => {
   return (
-    <div>TitleCards</div>
+    <div className='mt-[10px] mb-[30px] w-[100%]'>
+      <h2  className='mb-[8px] text-[20px]'>Popular on Netflix</h2>
+
+      <div className='flex gap-[10px] w-full'>
+        {
+          CardsData.map((card, index) => {
+            return (
+              <div style={{ width: '240px' }} className='relative' key={index}>
+                <img className='rounded-[4px] cursor-pointer' src={card.image} alt={card.img} />
+                <p>{card.name}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,19 +4,20 @@ import HeroBanner from '../../assets/assets/hero_banner.jpg'
 import HeroTitle from '../../assets/assets/hero_title.png'
 import PlayIcon from '../../assets/assets/play_icon.png';
 import InfoIcon from '../../assets/assets/info_icon.png';
+import TitleCards from '../../components/TitleCards/TitleCards';
 
 const Home = () => {
   return (
-    <div>
+    <div className='overflow-hidden'>
       <NavBar/>
       {/* HERO BANNER */}
       <div className="relative">
-      <div className="relative w-full h-full">
+      <div className="relative">
         <img className="w-full h-auto" src={HeroBanner} alt="Hero Banner" />
         <div className="absolute inset-0 bg-gradient-to-r from-black  to-transparent"></div>
       </div>
 
-      <div className="absolute w-full pl-[6%] bottom-[20px] font-Poppins text-white">
+      <div className="absolute pl-[6%] bottom-[20px] font-Poppins text-white">
         <img className='w-[90%] max-w-[420px] mb-[30px]' src={HeroTitle} alt="Hero Title" />
         <p className='max-w-[700px] text-[17px] mb-[20px]'>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
         {/* BUTTONS */}
@@ -24,6 +25,7 @@ const Home = () => {
           <button className='rounded-sm flex cursor-pointer items-center gap-[10px] text-[15px] py-[8px] bg-white font-[600] text-black px-[20px] border-none outline-none hover:bg-[#ffffffbf]'><img className='w-[25px]' src={PlayIcon} alt={PlayIcon} />Play</button>
           <button className='rounded-sm flex cursor-pointer items-center gap-[10px] text-[15px] py-[8px] bg-[#6d6d6eb3] font-[600] px-[20px] border-none outline-none hover:bg-[#6d6d6e66]'><img className='w-[25px]' src={InfoIcon} alt={InfoIcon} />More Info</button>
         </div>
+        <TitleCards/>
       </div>
     </div>
   </div>
